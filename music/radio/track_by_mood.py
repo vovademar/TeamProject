dedd75dcbb646e0131784ec8aabb6fe5bc0c76c4
vovademarnsu <v.medvedev@g.nsu.ru>
2022-11-24 12:18:25 +0700

    choose radio

--- music/radio/track_by_mood.py
+++ music/radio/track_by_mood.py
@@ -1,39 +1,40 @@
-
+from itertools import groupby
 from time import sleep
 
 from yandex_music import Client
 
-from radio import Radio
+from music.radio.radio import Radio
 
-# API instance
-client = Client(token='token')
 
-#sad music
-_stations = client.rotor_stations_list()
-_station = None
-for i in _stations:
-    _station = i.station
-    print(_station.id_for_from)
-    if _station.id_for_from == 'mood-sad':
+
+def choose_what_to_play(client):
+    type_list = []
+    tag_list = []
+    print("Choose type:")
+    _stations = client.rotor_stations_list()
+    for i in _stations:
         _station = i.station
-        break
+        type_list.append(_station.id.type)
+    new_type_list = [el for el, _ in groupby(type_list)]
+    print(new_type_list)
+    s_type = input()
 
-_station_id = f'{_station.id.type}:{_station.id.tag}'
-_station_from = _station.id_for_from
+    while s_type not in new_type_list:
+        print("incorrect input, try again:")
+        s_type = input()
 
-# Radio instance
-radio = Radio(client)
+    print("Choose tag:")
+    for i in _stations:
+        _station = i.station
+        if _station.id.type == s_type:
+            tag_list.append(_station.id.tag)
+    print(tag_list)
+    s_tag = input()
 
-# print id and for from id
-print(_station_id)
-print(_station_from)
+    while s_tag not in tag_list:
+        print("incorrect input, try again:")
+        s_tag = input()
 
-# start radio and get first track
-first_track = radio.start_radio(_station_id, _station_from)
-print('[Radio] First track is:', first_track)
+    _station_id = f'{s_type}:{s_tag}'
 
-# get new track every 5 sec
-while True:
-    sleep(5)
-    next_track = radio.play_next()
-    print('[Radio] Next track is:', next_track)
+    return _station_id
