06e57c5d0ff64ddf87c403b2b27462531c4de043
vovademar <v.medvedev@g.nsu.ru>
2023-05-10 14:55:26 +0700

    radiotracks

--- demo.py
+++ demo.py
@@ -5,22 +5,27 @@ from music.get_current_track import CurrentTrack
 from music.radio.track_by_mood import TrackByMood
 from music.radio.track_by_mood import play_next_radio_track
 
-
 token = ""
 print(token)
 
 client: Client = Client(token).init()
+
+# print(client.account_status)
 #
-# track_by_mood = TrackByMood(client)
-#
+#angry, disgust, fear, happy, neutral, sad, surprise
+#aggressive, haunting, dark, happy, dream, sad, epic
+track_by_mood = TrackByMood(client)
+print(track_by_mood.play_sad())
+# print(track_by_mood.get_chosen_tracks())
+
 # track_by_mood.radi()
 # rad = track_by_mood.radi()[1]
+# #
+# # print("next track:::")
+# # play_next_radio_track(rad)
 #
-# print("next track:::")
-# play_next_radio_track(rad)
-
-
-curTrack = CurrentTrack(client).get_label()
-CurrentTrack(client)
-
-print(curTrack)
+#
+# curTrack = CurrentTrack(client).get_label()
+# #CurrentTrack(client)
+#
+# print(curTrack)
