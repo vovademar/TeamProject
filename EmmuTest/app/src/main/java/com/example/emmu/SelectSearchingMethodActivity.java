ea302cacfc381b3d733f53f1de0b6824315f8931
Igor <igor777.fesh@mail.ru>
2023-06-01 17:19:57 +0700

    Обновил дизайн главного экрана

--- EmmuTest/app/src/main/java/com/example/emmu/SelectSearchingMethodActivity.java
+++ EmmuTest/app/src/main/java/com/example/emmu/SelectSearchingMethodActivity.java
@@ -1,9 +1,11 @@
 package com.example.emmu;
 
+import android.annotation.SuppressLint;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.RelativeLayout;
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -11,9 +13,10 @@ import com.example.emmu.CameraActivities.CameraActivity;
 
 public class SelectSearchingMethodActivity extends AppCompatActivity {
 
-    private Button findMusicByMoodsButton;
-    private Button findMusicByPhotoButton;
+    private RelativeLayout findMusicByMoodsButton;
+    private RelativeLayout findMusicByPhotoButton;
 
+    @SuppressLint("MissingInflatedId")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
